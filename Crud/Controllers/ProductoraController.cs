9125e9d5f9d81358213b907edee680cbbb5e26e0
Marcos <MarcosMaggiotti@hotmail.com.ar>
2023-12-14 12:14:22 -0300

    segundo commit

--- Crud/Controllers/ProductoraController.cs
+++ Crud/Controllers/ProductoraController.cs
@@ -7,29 +7,29 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 namespace Crud.Controllers
 {
-   
-        [ApiController]
-        [Route("api/[controller]")]
 
-        public class ProductoraController : ControllerBase
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ProductoraController : ControllerBase
+    {
+        private readonly MARCOS_1Context _1Context
+        ;
+        public ProductoraController(MARCOS_1Context context)
         {
-            private readonly MARCOS_1Context _1Context
-            ;
-            public ProductoraController(MARCOS_1Context context)
-            {
-                _1Context = context;
+            _1Context = context;
 
-            }
+        }
 
 
 
-            [HttpGet]
-            public IActionResult GetProductoraNameByName([FromQuery] int id )
-            {
-                //string Name = request.name;
-                //string Id = request.Idd;
+        [HttpGet]
+        public IActionResult GetProductoraNameByName([FromQuery] int id)
+        {
+            //string Name = request.name;
+            //string Id = request.Idd;
 
-                var result = _1Context.Productoras.FirstOrDefault(p => p.IdProductora == id);
+            var result = _1Context.Productoras.FirstOrDefault(p => p.IdProductora == id);
 
             if (result != null)
             {
@@ -40,13 +40,13 @@ namespace Crud.Controllers
                 return NotFound(new { Error = $"No se encontro el id: {id}" });
 
             }
-            }
+        }
 
- 
+
 
 
         [HttpGet("id")]
-        public IActionResult GetProductoraNameByName([FromRoute] GetProductoraRequest request )
+        public IActionResult GetProductoraNameByName([FromRoute] GetProductoraRequest request)
         {
             int skip = request.Skip;
             int take = request.Take;
@@ -64,7 +64,28 @@ namespace Crud.Controllers
         }
 
 
-        [HttpGet("cosa")]
+        [HttpGet("ProductoraPeliculas")]
+        public IActionResult GetProductoraPeliculasByName([FromRoute] int idProductora)
+        {
+            var productora = _1Context.Productoras.FirstOrDefault(p => p.IdProductora == idProductora);
+
+            if (productora == null) return NotFound(new { Error = $"No se pudo encontrar la productora {nombre}." });
+
+            var peliculas = _1Context.Peliculas
+                            .Where(p => p.IdProductora == productora.IdProductora)
+                            .Include(i => i.Titulo)
+                            .ToList();
+
+
+            if (peliculas == null) return NotFound(new { Error = $"No se encontraron peliculas para la productora {nombre}." });
+            return Ok(peliculas);
+
+                
+            
+
+
+        }
+
 
 
 
